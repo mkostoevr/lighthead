5cdefa35c53e10b00d1a77948a06503bbfaec130
Magomed Kostoev <mkostoevr@yandex.ru>
2024-08-03 18:42:23 +0300

    perf: introduce VebTree benchmarks
    These include the following methods:
    - contains
    - successor
    - predecessor
    - insert

---
+++ perf/vebtree.cc
@@ -0,0 +1,59 @@
+#include <random>
+
+#include <benchmark/benchmark.h>
+
+#include "perf.hh"
+
+#include "../VebTree.hh"
+
+template<typename KeyGen>
+static void
+vebtree_contains(benchmark::State &state, size_t count, KeyGen kg)
+{
+	VebTree t(32);
+	for (size_t i = 0; i < count; i++)
+		t.insert(i);
+	for (auto _ : state)
+		benchmark::DoNotOptimize(t.contains(kg()));
+}
+
+generate_benchmark_keys_size_iterations(vebtree_contains, 1000000);
+
+template<typename KeyGen>
+static void
+vebtree_successor(benchmark::State &state, size_t count, KeyGen kg)
+{
+	VebTree t(32);
+	for (size_t i = 0; i < count; i++)
+		t.insert(i);
+	for (auto _ : state)
+		benchmark::DoNotOptimize(t.successor(kg()));
+}
+
+generate_benchmark_keys_size_iterations(vebtree_successor, 1000000);
+
+template<typename KeyGen>
+static void
+vebtree_predecessor(benchmark::State &state, size_t count, KeyGen kg)
+{
+	VebTree t(32);
+	for (size_t i = 0; i < count; i++)
+		t.insert(i);
+	for (auto _ : state)
+		benchmark::DoNotOptimize(t.predecessor(kg()));
+}
+
+generate_benchmark_keys_size_iterations(vebtree_predecessor, 1000000);
+
+template<typename KeyGen>
+static void
+vebtree_insert(benchmark::State &state, size_t count, KeyGen kg)
+{
+	VebTree t(32);
+	for (auto _ : state)
+		t.insert(kg());
+}
+
+generate_benchmark_keys_size_iterations(vebtree_insert, 1000000);
+
+BENCHMARK_MAIN();
