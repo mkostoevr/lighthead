62cd15140a94187e6dcbccbcd0e9ef811cc70274
Magomed Kostoev <mkostoevr@yandex.ru>
2024-08-03 18:40:41 +0300

    all: introduce the van Emde Boas tree
    It supports the following set of operations:
    - contains: O(log(W))
    - insert: O(log(W))
    - erase: O(log(W))
    - get successor: O(log(W))
    - get precedessor: O(log(W))
    - get min: O(1)
    - get max: O(1)

---
+++ test/unit/vebtree.cc
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <cstdlib>
+#include <set>
+
+#include "unit.h"
+
+#include <VebTree.hh>
+
+#define test_key(tree, set, key) do {			\
+	if (set.find(key) != set.end())			\
+		fail_unless(tree.contains(key));	\
+	else						\
+		fail_unless(!tree.contains(key));	\
+} while (false)
+
+#define test_insert(tree, set, value) do {	\
+	test_key(tree, set, value);		\
+	set.insert(value);			\
+	tree.insert(value);			\
+	fail_unless(tree.contains(value));	\
+} while (false)
+
+#define test_erase(tree, set, value) do {		\
+	test_key(tree, set, value);			\
+	bool existed = set.erase(value);		\
+	fail_unless(tree.erase(value) == existed);	\
+	fail_unless(!tree.contains(value));		\
+} while (false)
+
+#define test_successor(tree, set, value) do {			\
+	test_key(tree, set, value);				\
+	auto it = set.upper_bound(value);			\
+	if (it != set.end()) {					\
+		fail_unless(tree.successor(value));		\
+		fail_unless(*tree.successor(value) == *it);	\
+	} else {						\
+		fail_unless(!tree.successor(value));		\
+	}							\
+} while (false)
+
+#define test_predecessor(tree, set, value) do {			\
+	test_key(tree, set, value);				\
+	auto it = set.lower_bound(value);			\
+	if (--it != set.end()) {				\
+		fail_unless(tree.predecessor(value));		\
+		fail_unless(*tree.predecessor(value) == *it);	\
+	} else {						\
+		fail_unless(!tree.predecessor(value));		\
+	}							\
+} while (false)
+
+int
+main()
+{
+	VebTree tree(32);
+	std::set<uint32_t> set;
+	const size_t count = 100000;
+
+	/* Insert some data. */
+	for (int i = 1; i < count; i++)
+		test_insert(tree, set, i * 2);
+
+	/* Check if the data is correct. */
+	fail_unless(!tree.contains(0));
+	fail_unless(!tree.contains(1));
+	for (int i = 1; i < count; i++) {
+		fail_unless(tree.contains(i * 2));
+		fail_unless(!tree.contains(i * 2 + 1));
+	}
+
+	/* Check the tree.successor(). */
+	for (int i = 0; i < count * 2; i++)
+		test_successor(tree, set, i);
+
+	/* Check the tree.predecessor(). */
+	for (int i = 0; i < count * 2; i++)
+		test_predecessor(tree, set, i);
+
+	/* Check the existing and unexisting data erase. */
+	for (int i = 0; i < count * 2; i++)
+		test_erase(tree, set, i);
+
+	/* Fill and clear the tree once again. */
+	for (int i = 1; i < count; i++)
+		test_insert(tree, set, i * 2);
+	for (int i = 1; i < count * 2; i++)
+		test_erase(tree, set, i);
+
+	return 0;
+}
